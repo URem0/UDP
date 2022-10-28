72bc93c3ccefbc0e1a6ce1568fab52606bc6c170
Remy UM <umremy0@gmail.com>
2022-10-28 11:02:57 +0200

    UDP server

--- src/UDPServer.java
+++ src/UDPServer.java
@@ -1,17 +1,12 @@
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 
 
 public class UDPServer {
 
     public int port;
     public String state;
-    private byte[] buf = new byte[256];
-
+    final byte[] buf = new byte[1024];
 
     public UDPServer(int port){
         this.port = port;
@@ -24,12 +19,20 @@ public class UDPServer {
 
     public void launch() throws IOException {
         DatagramSocket socket = new DatagramSocket(this.port);
-        DatagramPacket packet = new DatagramPacket(buf, buf.length);
-        socket.receive(packet);
 
-        String word = new String(packet.getData());
+        while (this.state.equals("listening")) {
+            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            socket.receive(packet);
+            String word = new String(packet.getData()).trim();
+            InetAddress address = packet.getAddress();
+            int port = packet.getPort();
 
-        System.out.println(word);
+            if (word.equals("exit")){
+                this.state = "close";
+            }
+            System.out.println(word + " from " + address + " port :" + port);
+        }
+        socket.close();
     }
 
     public static void main(String[] args) throws IOException {
@@ -38,6 +41,7 @@ public class UDPServer {
             p = Integer.parseInt(args[0]);
         }
         UDPServer server = new UDPServer(p);
+        server.state = "listening";
         server.launch();
     }
 }
